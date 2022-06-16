809010b29bd1f9d5a44c1967311ba15e20381087
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2022-06-16 09:33:06 +0300

    Added Unit Tests

--- src/main/java/com/bta/diplom/service/impl/ActivationLinkCleanServiceImpl.java
+++ src/main/java/com/bta/diplom/service/impl/ActivationLinkCleanServiceImpl.java
@@ -1,32 +1,28 @@
 package com.bta.diplom.service.impl;
 
-import com.bta.diplom.model.ActivationLink;
 import com.bta.diplom.repository.ActivationLinkRepository;
 import com.bta.diplom.service.ActivationLinkCleanService;
 import java.time.ZonedDateTime;
-import java.util.List;
 import javax.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 
 
 @Slf4j
-@Service
+@AllArgsConstructor
 public class ActivationLinkCleanServiceImpl implements ActivationLinkCleanService {
 
-  @Autowired
   private ActivationLinkRepository activationLinkRepository;
-
 
   @Scheduled(initialDelay = 5_000l, fixedRate = 10_000l)
   @Transactional
   @Override
   public void clean() {
-    final var checkDate = ZonedDateTime.now().minusDays(1l);
+    final var checkDate = ZonedDateTime.now().minusMinutes(3l);
     final var expiredLinks = activationLinkRepository.findByCreatedBefore(checkDate);
-    if (expiredLinks.isEmpty()) {
+    if (expiredLinks == null || expiredLinks.isEmpty()) {
       log.info("No expired Activation Links in System.");
       return;
     }
---
+++ src/test/java/com/bta/diplom/service/impl/ActivationLinkCleanServiceImplTest.java
@@ -0,0 +1,71 @@
+package com.bta.diplom.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.bta.diplom.model.ActivationLink;
+import com.bta.diplom.repository.ActivationLinkRepository;
+import java.util.Collections;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ActivationLinkCleanServiceImplTest {
+
+  private ActivationLinkRepository activationLinkRepository;
+
+  private ActivationLinkCleanServiceImpl instanceUnderTest;
+
+
+  @BeforeEach
+  void beforeEach() {
+    activationLinkRepository = mock(ActivationLinkRepository.class);
+    instanceUnderTest = new ActivationLinkCleanServiceImpl(activationLinkRepository);
+  }
+
+  @Test
+  public void testIfNoExpiredActivationLinks() {
+    //given
+    when(activationLinkRepository.findByCreatedBefore(any())).thenReturn(Collections.emptyList());
+
+    //when
+    instanceUnderTest.clean();
+
+    //then
+    verify(activationLinkRepository, never()).deleteAll(any());
+  }
+
+  @Test
+  public void testIfExpiredActivationLinks() {
+    //given
+    when(activationLinkRepository.findByCreatedBefore(any()))
+        .thenReturn(Collections.singletonList(
+            ActivationLink.builder().build())
+        );
+
+    //when
+    instanceUnderTest.clean();
+
+    //then
+    verify(activationLinkRepository, times(1)).deleteAll(any());
+  }
+
+  @Test
+  public void testIfExpiredActivationLinksAreNull() {
+    //given
+    when(activationLinkRepository.findByCreatedBefore(any()))
+        .thenReturn(null);
+
+    //when
+    instanceUnderTest.clean();
+
+    //then
+    verify(activationLinkRepository, times(0)).deleteAll(any());
+  }
+
+
+}
